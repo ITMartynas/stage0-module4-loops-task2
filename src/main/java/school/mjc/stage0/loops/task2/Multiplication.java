b5945cb382a0c7c88a11d4ae446420a836c391f2
Martynas <martynas.bartnykas@gmail.com>
2024-01-08 19:32:23 +0200

    while tasks

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -1,6 +1,23 @@
 package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
-    public void printMultiplied(int multiplyByAndToInclusive) {
+    public void printMultiplied(int multiplyByAndToInclusive){
+        int i = 0;
+
+        if (multiplyByAndToInclusive > i){
+            while (i <= multiplyByAndToInclusive) {
+                int result = i * multiplyByAndToInclusive;
+                System.out.println(result);
+                i += 1;}
+        } else if (multiplyByAndToInclusive < i) {
+            while (i >= multiplyByAndToInclusive) {
+                int result = i * multiplyByAndToInclusive;
+                System.out.println(-result);
+                i--;}
+        }
+
     }
+
 }
+
+
--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
+        int i = 0;
+        StringBuilder phrase = new StringBuilder();
 
+        while (i < chars.length){
+            phrase.append(chars[i]);
+            i++;
+        }
+
+        System.out.print(phrase);
     }
 }
